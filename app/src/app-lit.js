0fbc7887d05a336534d98650ac1354d42509816e
Niklas <niklas.recordevolution@record-evolution.de>
2018-12-07 09:49:54 +0100

    added text and adjusted all elements to have same content

--- app/src/app-lit.js
+++ app/src/app-lit.js
@@ -1,4 +1,4 @@
-import { html, render } from 'lit-html';
+import { html, render } from '../node_modules/lit-html';
 
 class AppLit extends HTMLElement {
     constructor() {
@@ -11,13 +11,16 @@ class AppLit extends HTMLElement {
 
         const printElementName = (elem) => html `
         	<style>
-        		.color{color:red;}
+        		.color{color:red;font-size:35px;}
         	</style>
-        	<div class="color" style="font-size:35px;">
-        		Native JS Webcomponent - ${elem}!
-        	</div>
+        	<section class="color">
+        		${elem}
+        	</section>
+            <section>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+            </section>
         `;
-        render(printElementName('Lit-HTML'), shadow);
+        render(printElementName(this.getAttribute('text')), shadow);
 
     }
 }
--- app/src/app-native.js
+++ app/src/app-native.js
@@ -6,9 +6,12 @@ class AppNative extends HTMLElement {
     // Create a shadow root
 	var shadow = this.attachShadow({mode: 'open'});
 	//content
-    var info = document.createElement('span');
+    var info = document.createElement('section');
 	info.setAttribute('class','info');
 	info.textContent = this.getAttribute('text');
+
+	var text = document.createElement('section');
+	text.innerHTML = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,'
 	//Style
 	var style = document.createElement('style');
 	style.textContent = '.info {' +
@@ -19,6 +22,7 @@ class AppNative extends HTMLElement {
 	// append
 	shadow.appendChild(style);
 	shadow.appendChild(info);
+	shadow.appendChild(text);
 
   }
 }
